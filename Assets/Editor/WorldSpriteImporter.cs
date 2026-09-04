a7e6432d29fc5eb841d3a147c64a9f6e5816f2d7
Xunn3714 <miao371459@gmail.com>
2026-09-04 08:20:25 -0400

    Added Tilemap(manual), and WorldSpriteImporter for set the tile png.
    目前tilemap为占位图，后期可替换

---
+++ Assets/Editor/WorldSpriteImporter.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+
+/// 本文件用于自动设定Tilemap美术图片文件的像素及格式
+public class WorldSpriteImporter : AssetPostprocessor
+{
+    private const string TargetPath = "Art/WorldSprites";
+    private const float PixelsPerUnit = 128f;
+
+    void OnPreprocessTexture()
+    {
+        if (!assetPath.Contains(TargetPath)) return;
+
+        var importer = (TextureImporter)assetImporter;
+        importer.textureType = TextureImporterType.Sprite;
+        importer.filterMode = FilterMode.Bilinear;
+        importer.spritePixelsPerUnit = PixelsPerUnit;
+
+        importer.textureCompression = TextureImporterCompression.Compressed;
+        importer.mipmapEnabled = false; // 2D 游戏一般不需要 mipmap，除非会做大幅缩放远近效果
+    }
+}
